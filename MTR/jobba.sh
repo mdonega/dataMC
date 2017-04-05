96e6905c82c4e0a92bb6f8da4dcb96ec358be783
Mauro Donega <mauro.donega@cern.ch>
2017-04-05 16:30:44 +0200

    changed mc, added weights, EB/EE

--- MTR/jobba.sh
+++ MTR/jobba.sh
@@ -11,3 +11,5 @@ echo JOBBA: Training on $1 for Y = $2 , quantile = $3,  startEvt = $4, stopEvt =
 python /mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/train_quantileRegression_Batch.py $1 $2 $3 $4 $5 $6 $7 $8
 
 echo JOBBA DONE: Training on $1 for Y = $2 , quantile = $3,  startEvt = $4, stopEvt = $5, maxDepth = $6, minLeaf = $7, EBEE = $8
+
+
--- MTR/train_quantileRegression_Batch.py
+++ MTR/train_quantileRegression_Batch.py
@@ -2,10 +2,6 @@ import sys
 import os
 from quantileRegression import quantileRegression
 
-# Nevt passing selection
-# mc   4545316 : train on 2000000
-# data 1441902 : train on all
-
 class mycolors:
    red = '\033[91m'
    green = '\033[92m'
@@ -18,7 +14,7 @@ class mycolors:
    black = '\033[90m'
    default = '\033[0m'
 
-
+print "sys.argv", sys.argv
 print mycolors.green+"Training quantile regressions on"+mycolors.default
 dataMC = sys.argv[1]
 print "Data/MC = ", dataMC
@@ -31,9 +27,13 @@ stopEvt  = int(sys.argv[5])
 print "Events in [", startEvt, ", ", stopEvt, "]"
 imaxDepth  = int(sys.argv[6])
 iminLeaf   = int(sys.argv[7])
-sEBEE      = sys.argv[8]
+print "maxDepth / minLeaf = ", imaxDepth, " , " , iminLeaf
+sEBEE = ""
+if len(sys.argv) == 9 : 
+   sEBEE      = sys.argv[8]
+print "EB/EE = ", sEBEE
 
-# qr = quantileRegression(sys.argv[1])
+qr = quantileRegression(sys.argv[1])
 
 outputDir = "/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/weights_MaxDepth_" + str(imaxDepth) + "_minLeaf_" + str(iminLeaf)
 if (not os.path.exists(outputDir)):
@@ -41,41 +41,27 @@ if (not os.path.exists(outputDir)):
    os.mkdir(outputDir)
 
 if dataMC == "data":
-#   qr.loadDF("/mnt/t3nfs01/data01/shome/mdonega/dataMC/nt/double_ele_spring16v2_sync_v1_ichep/",
-#             "cicNoSigmaIetaIeta/trees/",
-#             ["Data_13TeV_EBHighR9", "Data_13TeV_EBLowR9", "Data_13TeV_EEHighR9", "Data_13TeV_EELowR9" ],
-#             startEvt, stopEvt, 12345)
-
-#   # to reduce memory consuption just load the locally pre-made h5 file
-#   qr.loadDFh5("/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/df_data_0-5491556.h5", startEvt, stopEvt)
-#   for q in quantiles:
-#      qr.trainQuantile(Y, q, outputDir, maxDepth = imaxDepth, minLeaf = iminLeaf)
-
+   #qr.loadDF("/mnt/t3nfs01/data01/shome/mdonega/lxplus/work/CMSSW_8_0_26_patch1/src/diphotons/Analysis/macros/double_ele_v9_moriond17_mc/",
+   #          "cicNoSigmaIetaIeta/trees/",
+   #          ["Data_13TeV_EBHighR9", "Data_13TeV_EBLowR9", "Data_13TeV_EEHighR9", "Data_13TeV_EELowR9" ],
+   #          startEvt, stopEvt, 12345)
+   # to reduce memory consuption just load the locally pre-made h5 file
+   qr.loadDFh5("/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/df_data_0-2000000.h5", startEvt, stopEvt)
    for q in quantiles:
-
-      qr = quantileRegression(sys.argv[1])
-      # to reduce memory consuption just load the locally pre-made h5 file
-      qr.loadDFh5("/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/df_data_0-5491556.h5", startEvt, stopEvt)
-      qr.trainQuantile(Y, q, outputDir, EBEE = sEBEE, maxDepth = imaxDepth, minLeaf = iminLeaf)
+      # qr.trainQuantile(Y, q, outputDir, maxDepth = imaxDepth, minLeaf = iminLeaf)
+      qr.trainQuantile(Y, q, outputDir, EBEE = sEBEE, maxDepth = imaxDepth, minLeaf = iminLeaf, useWeights=True)
 
 elif dataMC == "mc":
-#   qr.loadDF("/mnt/t3nfs01/data01/shome/mdonega/dataMC/nt/double_ele_spring16v2_sync_v1_mc/",
-#             "cicNoSigmaIetaIeta/trees/",
-#             ["DYToEE_powheg_13TeV_EBHighR9", "DYToEE_powheg_13TeV_EBLowR9", "DYToEE_powheg_13TeV_EEHighR9", "DYToEE_powheg_13TeV_EELowR9" ],
-#             startEvt, stopEvt, 12345)
-
-#   # to reduce memory consuption just load the locally pre-made h5 file
-#   qr.loadDFh5("/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/df_mc_0-2000000.h5", startEvt, stopEvt)
-#   for q in quantiles:
-#      qr.trainQuantile(Y, q, outputDir, maxDepth = imaxDepth, minLeaf = iminLeaf)
-
+   #qr.loadDF("/mnt/t3nfs01/data01/shome/mdonega/lxplus/work/CMSSW_8_0_26_patch1/src/diphotons/Analysis/macros/double_ele_v9_moriond17_mc/",
+   #          "cicNoSigmaIetaIeta/trees/",
+   #          ["DYJetsToLL_13TeV_EBHighR9", "DYJetsToLL_13TeV_EBLowR9", "DYJetsToLL_13TeV_EEHighR9", "DYJetsToLL_13TeV_EELowR9" ],
+   #          startEvt, stopEvt, 12345)
+   # to reduce memory consuption just load the locally pre-made h5 file
+   qr.loadDFh5("/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/df_mc_0-2000000.h5", startEvt, stopEvt)   
    for q in quantiles:
-      qr = quantileRegression(sys.argv[1])
-      # to reduce memory consuption just load the locally pre-made h5 file
-      qr.loadDFh5("/mnt/t3nfs01/data01/shome/mdonega/dataMC/MTR/df_mc_0-2000000.h5", startEvt, stopEvt)
-      qr.trainQuantile(Y, q, outputDir, EBEE = sEBEE, maxDepth = imaxDepth, minLeaf = iminLeaf)
-
-
+      # qr.trainQuantile(Y, q, outputDir, maxDepth = imaxDepth, minLeaf = iminLeaf)
+      qr.trainQuantile(Y, q, outputDir, EBEE = sEBEE, maxDepth = imaxDepth, minLeaf = iminLeaf, useWeights=True)
+      
 else: print " ERROR: choose data or mc"
 
 
